7b406d8f703742c80bc4a18c748bd1f570b469ba
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2026-04-01 14:13:19 -0500

    fix(layout): remove redundant fonts section from app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link href="{{ asset('css/simulation.css') }}" rel="stylesheet">
 
-        <!-- Fonts -->
-
-
         @stack('styles')
     </head>
 
